9ea0f98d0c3f2b4116e1313cdb2de13fbfd78463
Mai Mee <maimee2000@gmail.com>
2019-08-19 17:15:39 +0700

    Fixed doctest and added an error case

--- listutil.py
+++ listutil.py
@@ -11,9 +11,13 @@ def unique(lst: list) -> list:
     >>> unique([5])
     [5]
     >>> unique(["b","a","a","b","b","b","a","a"])
-    ["b","a"]
+    ['b', 'a']
     >>> unique([])
     []
+    >>> unique(5)
+    Traceback (most recent call last):
+      ...
+    TypeError: 'int' is not a list
     """
     if not isinstance(lst, list):
         raise TypeError(f"{str(lst.__class__)[7:-1]} is not a list")
